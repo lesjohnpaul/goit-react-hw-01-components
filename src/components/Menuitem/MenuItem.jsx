7f8f50d271befecf52bffdf3a3c28407e33ae614
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-03-04 16:35:24 +0800

    practice

---
+++ src/components/Menuitem/MenuItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const MenuItem = () => {
+  return (
+<div style={{marginBottom:'20px'}}>
+  <img src="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/08/Roasted-Beet-Salad-11.jpg"
+
+  alt="salad"
+  style={{height:'400px'}}
+  />
+
+    <h2>Green Salad</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Aliquid quod reiciendis, consectetur itaque enim animi provident
+      debitis nam quos ducimus odit et architecto das,
+      error odio a, iste doloremque!
+    </p>
+</div>
+  );
+};
+export default MenuItem;
