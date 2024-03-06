63836c73080a46826e0549334ab54ecc338d9be9
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-03-06 16:08:06 +0800

    userLoggedin

--- src/components/MenuItemList/MenuItemList.jsx
+++ src/components/MenuItemList/MenuItemList.jsx
@@ -1,18 +1,45 @@
+import PropTypes from 'prop-types';
 import MenuItem from 'components/MenuItem/MenuItem';
 
 export const MenuItemList = ({ salads }) => {
+  const styles = {
+    container: { padding: '20px' },
+    loginMessage: { fontSize: '18px', color: 'red', fontWeight: 'bold' },
+  };
+
+  const userLoggedIn = true;
   return (
-    <div>
-      {salads.map(({ name, description, imageUrl, price }) => (
-        <MenuItem
-          key={salads.name}
-          name={salads.name}
-          description={salads.description}
-          imageUrl={salads.imageUrl}
-          price={salads.price.individual}
-        />
-      ))}
+    <div style={styles.container}>
+      {userLoggedIn ? (
+        salads.map(({ name, description, imageUrl, price }) => (
+          <MenuItem
+            key={name}
+            name={name}
+            description={description}
+            imageUrl={imageUrl}
+            price={price}
+          />
+        ))
+      ) : (
+        <p style={styles.loginMessage}>
+          user needs to login first before being able to access the menu items.
+        </p>
+      )}
     </div>
   );
 };
+
+MenuItemList.propTypes = {
+  salads: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string.isRequired,
+      price: PropTypes.shape({
+        individual: PropTypes.number.isRequired,
+      }).isRequired,
+    })
+  ).isRequired,
+};
+
 export default MenuItemList;
