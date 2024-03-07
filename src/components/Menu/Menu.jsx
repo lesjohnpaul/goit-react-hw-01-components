b50a059952cb063c0597ac7bde8cb688c0292857
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-03-07 22:54:04 +0800

    add comments

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 // Menu.js
 import React from 'react';
 import PropTypes from 'prop-types';
-import css from './Menu.module.css'; // Assume some basic styling for the Menu component
+import css from './Menu.module.css'; // Assume some basic styling for the Menu componen
 
 const Menu = ({ title, children }) => {
   return (
