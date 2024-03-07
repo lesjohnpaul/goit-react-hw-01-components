96759307ceb6093d134bbad11c8fe8ac3fc1433e
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-03-07 14:59:16 +0800

    vanilla CSS

--- src/components/MenuItemList/MenuItemList.jsx
+++ src/components/MenuItemList/MenuItemList.jsx
@@ -1,15 +1,11 @@
 import PropTypes from 'prop-types';
 import MenuItem from 'components/MenuItem/MenuItem';
+import './MenuItemList.css';
 
 export const MenuItemList = ({ salads }) => {
-  const styles = {
-    container: { padding: '20px' },
-    loginMessage: { fontSize: '18px', color: 'red', fontWeight: 'bold' },
-  };
-
   const userLoggedIn = true;
   return (
-    <div style={styles.container}>
+    <div className="menuItemListContainer">
       {userLoggedIn ? (
         salads.map(({ name, description, imageUrl, price, availability }) => (
           <MenuItem
@@ -22,7 +18,7 @@ export const MenuItemList = ({ salads }) => {
           />
         ))
       ) : (
-        <p style={styles.loginMessage}>
+        <p>
           user needs to login first before being able to access the menu items.
         </p>
       )}
@@ -39,8 +35,54 @@ MenuItemList.propTypes = {
       price: PropTypes.shape({
         individual: PropTypes.number.isRequired,
       }).isRequired,
+      availability: PropTypes.string.isRequired,
     })
   ).isRequired,
 };
 
 export default MenuItemList;
+
+//================================INLINE CSS APPROACH======================
+// export const MenuItemList = ({ salads }) => {
+//   const styles = {
+//     container: { padding: '20px' },
+//     loginMessage: { fontSize: '18px', color: 'red', fontWeight: 'bold' },
+//   };
+
+//   const userLoggedIn = true;
+//   return (
+//     <div style={styles.container}>
+//       {userLoggedIn ? (
+//         salads.map(({ name, description, imageUrl, price, availability }) => (
+//           <MenuItem
+//             key={name}
+//             name={name}
+//             description={description}
+//             imageUrl={imageUrl}
+//             price={price}
+//             availability={availability}
+//           />
+//         ))
+//       ) : (
+//         <p style={styles.loginMessage}>
+//           user needs to login first before being able to access the menu items.
+//         </p>
+//       )}
+//     </div>
+//   );
+// };
+
+// MenuItemList.propTypes = {
+//   salads: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       name: PropTypes.string.isRequired,
+//       description: PropTypes.string.isRequired,
+//       imageUrl: PropTypes.string.isRequired,
+//       price: PropTypes.shape({
+//         individual: PropTypes.number.isRequired,
+//       }).isRequired,
+//     })
+//   ).isRequired,
+// };
+
+// export default MenuItemList;
--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,57 +1,103 @@
+import React from 'react';
 import PropTypes from 'prop-types';
+import { AiFillCheckCircle } from 'react-icons/ai';
+import './MenuItem.css';
 
-const getBgColor = availability => {
+const getClassNameForAvailability = availability => {
   switch (availability) {
     case 'Available':
-      return '#C5e8d7';
+      return 'available';
     case 'Few Left':
-      return '#F7e2bc';
+      return 'fewLeft';
     case 'Not Available':
-      return '#e8c5c6';
+      return 'notAvailable';
     case 'Seasonal':
-      return '#c5d4e8';
+      return 'seasonal';
     default:
-      throw new Error(`Unsupported availability Status - ${availability}`);
+      return '';
   }
 };
+//================================INLINE CSS APPROACH====================
+// const getBgColor = availability => {
+//   switch (availability) {
+//     case 'Available':
+//       return '#C5e8d7';
+//     case 'Few Left':
+//       return '#F7e2bc';
+//     case 'Not Available':
+//       return '#e8c5c6';
+//     case 'Seasonal':
+//       return '#c5d4e8';
+//     default:
+//       throw new Error(`Unsupported availability Status - ${availability}`);
+//   }
+// };
 
 const MenuItem = ({ name, description, imageUrl, price, availability }) => {
-  //inline styles for each element
-  const styles = {
-    container: {
-      marginBottom: '20px',
-      border: '1px solid #ccc',
-      padding: '10px',
-      borderRadius: '5px',
-      maxWidth: '400px',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      backgroundColor: getBgColor(availability),
-    },
-    image: {
-      height: '400px',
-      width: '100%',
-      objectFit: 'cover',
-      marginBottom: '10px',
-    },
-    name: { fontsize: '24px', fontWeight: 'bold', marginBottom: '10px' },
-    description: { fontSize: '16px', color: '#666', marginBottom: '10px' },
-    price: { fontSize: '16px', color: '#333' },
-    priceSmall: { fontSize: '15px', color: '#313' },
-  };
+  const availabilityClass = getClassNameForAvailability(availability);
 
   return (
-    <div style={styles.container}>
-      <img src={imageUrl} alt={name} style={styles.image} />
-      <h2 style={styles.name}>{name}</h2>
-      <p style={styles.description}>{description}</p>
-      <p style={styles.price}>Price: ${price.individual.toFixed(2)}</p>
-      <p style={styles.priceSmall}>Price of Small ${price.Small.toFixed(2)}</p>
-      <p>{availability}</p>
+    <div className={`menuItemContainer ${availabilityClass}`}>
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`menutItemImage ${availabilityClass}`}
+      />
+      <h2 className="menuItemName">
+        <span className={`menuItemIcon ${availabilityClass}`}>
+          <AiFillCheckCircle />
+        </span>
+        {name}
+      </h2>
+      <p className={`menuItemDescription ${availabilityClass}`}>
+        {description}
+      </p>
+      <p className={`menuItemPrice ${availabilityClass}`}>
+        Price: ${price.individual.toFixed(2)}
+      </p>
+      <p className={`menuItemPrice ${availabilityClass}`}>
+        Price of Small ${price.Small.toFixed(2)}
+      </p>
     </div>
   );
 };
 
+//================================INLINE CSS APPROACH======================
+// const MenuItem = ({ name, description, imageUrl, price, availability }) => {
+//inline styles for each element
+//  const styles = {
+//    container: {
+//      marginBottom: '20px',
+//      border: '1px solid #ccc',
+//      padding: '10px',
+//      borderRadius: '5px',
+//      maxWidth: '400px',
+//      marginLeft: 'auto',
+//      marginRight: 'auto',
+//      backgroundColor: getBgColor(availability),
+//    },
+//    image: {
+//      height: '400px',
+//      width: '100%',
+//      objectFit: 'cover',
+//      marginBottom: '10px',
+//    },
+//    name: { fontsize: '24px', fontWeight: 'bold', marginBottom: '10px' },
+//    description: { fontSize: '16px', color: '#666', marginBottom: '10px' },
+//    price: { fontSize: '16px', color: '#333' },
+//    priceSmall: { fontSize: '15px', color: '#313' },
+//  };
+// return (
+//     <div style={styles.container}>
+//       <img src={imageUrl} alt={name} style={styles.image} />
+//       <h2 style={styles.name}>{name}</h2>
+//       <p style={styles.description}>{description}</p>
+//       <p style={styles.price}>Price: ${price.individual.toFixed(2)}</p>
+//       <p style={styles.priceSmall}>Price of Small ${price.Small.toFixed(2)}</p>
+//       <p>{availability}</p>
+//     </div>
+//   );
+// };
 MenuItem.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
